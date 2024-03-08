d29cd8b8a40d147fe78073942f37696c3bbb27e9
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-03-08 16:06:50 -0600

    stringbuffer

---
+++ StringBuffer.java
@@ -0,0 +1,21 @@
+public class StringBuffer {
+    public StringBuffer(String hello) {
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        StringBuffer buffer = new StringBuffer("Hello ");
+        System.out.println();
+        buffer.append("World");
+        System.out.println(buffer);
+
+
+
+    }
+
+    private void append(String world) {
+
+        
+    }
+
+}
