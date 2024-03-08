907e21f4f7e079598f6d5e5c0e2d54ac0c1df7e0
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-03-08 16:24:53 -0600

    notes

--- ListTypes.java
+++ ListTypes.java
@@ -7,6 +7,8 @@ public class ListTypes {
     public static void main(String[] args) {
         List<Integer> linkedList = new LinkedList<Integer>();
         List<Integer> arrayList = new ArrayList<Integer>();
+            //faster, takes less space
         List<Integer> vectors = new Vector<Integer>();
+            //thread-safe, slower than arraylist
     }
 }
